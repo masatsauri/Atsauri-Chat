19039cbe4238b136ed8f97911f93bfcf761b0069
masatsauri <ma_atsauri@yahoo.com>
2015-07-03 00:15:24 +0700

    Create index_header.php

---
+++ index_header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- view port, melihat ukuran layar device -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Judul -->
+    <title>Simple Messanging System</title>
+    <!-- Bootstrap -->
+    <link href="view/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link href="view/fa/css/font-awesome.min.css" rel="stylesheet">
+    <!-- CSS anda -->
+    <link href="view/css/index_style.css" rel="stylesheet">
+</head>
+<body>
