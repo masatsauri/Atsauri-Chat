7d1f7711d7ba337c0d51e493c1847b828184f9e8
masatsauri <ma_atsauri@yahoo.com>
2015-07-03 00:17:15 +0700

    Create index_footer.php

---
+++ index_footer.php
@@ -0,0 +1,6 @@
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="view/js/jquery.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="view/bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
